c294d0d6b0f022b6925c0090a00395ca74c46a96
rriese <rafael.riese@catolicasc.org.br>
2023-04-24 21:43:45 -0300

    Adicionado uploader de imagens

--- src/components/Modal/claim.js
+++ src/components/Modal/claim.js
@@ -1,13 +1,15 @@
 import { useState } from "react";
-import { Modal, Input, Cascader, Spin } from 'antd';
+import { Modal, Input, Cascader, Spin, Upload, Button, Divider } from 'antd';
 import { toast } from "react-toastify";
 import ServiceBase from "../../services/serviceBase";
 import { useNavigate, useLocation } from "react-router-dom";
+import { UploadOutlined } from '@ant-design/icons';
 import Utils from "../../services/utils";
 
 const { TextArea } = Input;
 
 const ClaimModal = ({ isModalOpen, setIsModalOpen, dataEdit, getClaims }) => {
+    const [fileList, setFileList] = useState([]);
     const [id, setId] = useState((dataEdit && dataEdit.id) || "");
     const [title, setTitle] = useState((dataEdit && dataEdit.title) || "");
     const [description, setDescription] = useState((dataEdit && dataEdit.description) || "");
@@ -31,6 +33,15 @@ const ClaimModal = ({ isModalOpen, setIsModalOpen, dataEdit, getClaims }) => {
     };
 
     const handleOk = async () => {
+        // alert('Vamos testar o upload!');
+
+        // const formData = new FormData();
+        // fileList.forEach((file) => {
+        //     formData.append('files[]', file);
+        // });
+
+        // let serviceResponse = await ServiceBase.postRequest('api/image/643ddcbacd5875259347636c/', formData);
+
         if (!title ||
             !description ||
             !stateAndCity[0] ||
@@ -85,6 +96,25 @@ const ClaimModal = ({ isModalOpen, setIsModalOpen, dataEdit, getClaims }) => {
         }
     };
 
+    const uploaderProps = {
+        // multiple: true,
+        // maxCount: 3,
+
+        onRemove: (file) => {
+            const index = fileList.indexOf(file);
+            const newFileList = fileList.slice();
+            newFileList.splice(index, 1);
+            setFileList(newFileList);
+        },
+
+        beforeUpload: (file) => {
+            alert("BEFORE");
+            setFileList([...fileList, file]);
+            return false;
+        },
+        fileList
+    }
+
     return (
         <Modal title="Reclamação" open={isModalOpen} onOk={handleOk} onCancel={handleCancel}>
             <Spin spinning={loading} size="large">
@@ -99,6 +129,12 @@ const ClaimModal = ({ isModalOpen, setIsModalOpen, dataEdit, getClaims }) => {
                 </span>
                 &nbsp;
                 <Input value={district} onChange={(e) => setDistrict(e.target.value)} placeholder="Bairro" />
+                &nbsp;
+                <div>
+                    <Upload {...uploaderProps}>
+                        <Button icon={<UploadOutlined />}>Anexar imagens</Button>
+                    </Upload>
+                </div>
             </Spin>
         </Modal>
     )
