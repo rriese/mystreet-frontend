0e38d340a2602ad75b9ab0f06a7183241e9bf5e2
rriese <rafael.riese@catolicasc.org.br>
2023-05-10 19:39:31 -0300

    Alterado lógica de geração de relatório

--- src/pages/Reports/index.js
+++ src/pages/Reports/index.js
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { Card, Select, Button } from 'antd';
+import { Card, Select, Button, Spin } from 'antd';
 import { toast } from "react-toastify";
 import ServiceBase from '../../services/serviceBase';
 
@@ -7,48 +7,56 @@ const OPTIONS = ['Usuários cadastrados', 'Reclamações cadastradas'];
 
 function Reports() {
     const [selectedReports, setSelectedReports] = useState([]);
+    const [loading, setLoading] = useState(false);
     const filteredOptions = OPTIONS.filter((o) => !selectedReports.includes(o));
 
     const handleGenerate = async () => {
         if (selectedReports.length === 0) {
             toast.warn('Selecione ao menos 1 relatório.');
         } else {
+            setLoading(true);
             for (const report of selectedReports) {
-                let url = 'api/report/excel/';
-                let name = '';
-                if (report === 'Usuários cadastrados') {
-                    name = 'users'
-                } else if (report === 'Reclamações cadastradas') {
-                    name = 'claims';
-                }
-                url += name;
+                let url = '';
 
-                var link = document.createElement('a');
-                link.href = ServiceBase.getBaseUrl() + url;
-                link.download = name;
-                link.setAttribute('target', '_blank');
-                link.click();
-                link.remove();
+                if (report === 'Usuários cadastrados') {
+                    url = 'api/report/excel/users/generate';
+                } else if (report === 'Reclamações cadastradas') {
+                    url = 'api/report/excel/claims/generate';
+                }
+
+                let serviceResponse = await ServiceBase.getRequest(url);
+
+                if (serviceResponse && serviceResponse.responseType === 'OK') {
+                    var link = document.createElement('a');
+                    link.href = ServiceBase.getBaseUrl() + 'api/report/export/' + serviceResponse.content;
+                    link.setAttribute('target', '_blank');
+                    link.download = serviceResponse.content;
+                    link.click();
+                    link.remove();
+                }
             }
+            setLoading(false);
         }
     }
 
     return (
-        <Card title="Relatórios">
-            <Select
-                mode="multiple"
-                placeholder="Relatórios disponíveis"
-                value={selectedReports}
-                onChange={setSelectedReports}
-                style={{ width: '100%' }}
-                options={filteredOptions.map((item) => ({
-                    value: item,
-                    label: item,
-                }))}
-            />
-            &nbsp;
-            <Button style={{ width: '100%' }} type="primary" onClick={handleGenerate}>Gerar relatórios</Button>
-        </Card>
+        <Spin spinning={loading} size="large">
+            <Card title="Relatórios">
+                <Select
+                    mode="multiple"
+                    placeholder="Relatórios disponíveis"
+                    value={selectedReports}
+                    onChange={setSelectedReports}
+                    style={{ width: '100%' }}
+                    options={filteredOptions.map((item) => ({
+                        value: item,
+                        label: item,
+                    }))}
+                />
+                &nbsp;
+                <Button style={{ width: '100%' }} type="primary" onClick={handleGenerate}>Gerar relatórios</Button>
+            </Card>
+        </Spin>
     )
 }
 
